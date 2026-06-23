bc263957122156ab6e5b88dd540117d579aba4b5
Adnan Shaikh <shaikhaadnan02@gmail.com>
2026-06-23 19:56:01 +0530

    put created

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -2,6 +2,7 @@ const express=require("express");
 const router=express.Router();
 const Project=require("../models/project");
 const auth=require("../middleware/auth");
+const project = require("../models/project");
 router.post("/create",auth,async(req,res)=>{
     try{
         const {title,description,budget}=req.body;
@@ -26,4 +27,26 @@ router.get("/my-projects",auth,async(req,res)=>{
         res.status(500).json({error:err.msg});
     }
 });
-router.get("/all",auth,async(req,res))
+router.get("/all",auth,async(req,res)=>{
+    try{
+        const projects=await Project.find().populate("client","name email");
+        res.json(projects);
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+});
+router.put("/update/:id",auth,async(req,res)=>{
+    try{
+        const {status}=req.body;
+        const project=await Project.findByIdAndUpdate(
+            req.params.id,
+            {status},
+            {new:true}
+        );
+        res.json(project);
+    }
+    catch(err){
+        res.status(500).json({error:err.message});
+    }
+});
+module.exports=router;
