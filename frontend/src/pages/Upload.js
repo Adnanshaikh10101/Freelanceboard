5395ed494550e0c94a3e51a6705c5ccff6354727
Adnan Shaikh <shaikhaadnan02@gmail.com>
2026-06-28 15:46:36 +0530

    Upload frontend Cpmpleted

---
+++ frontend/src/pages/Upload.js
@@ -0,0 +1,25 @@
+import React,{useState} from "react";
+import API from "../services/api";
+function Upload(){
+    const [file,setfile] = useState(null);
+    const handleupload = async()=>{
+        const formData = new formData();
+        formData.append("file",file);
+        try{
+            const res = await API.post("/upload",formData);
+            alert("Uploaded Successfully");
+            console.log(res.data);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+};
+return(
+    <div className="Upload">
+        <h2>Upload File</h2>
+        <input type="file" onChange={(e)=> setfile(e.target.files[0])}/>
+        <button onClick={handleupload}>Upload</button>
+    </div>
+);
+export default Upload;
