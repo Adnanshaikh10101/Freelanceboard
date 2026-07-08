106194c8914ed19db76c75d6bdf825e7c42f8f71
Adnan Shaikh <shaikhaadnan02@gmail.com>
2026-07-08 14:14:43 +0530

    Admin updated

--- frontend/src/pages/Navbar.js
+++ frontend/src/pages/Navbar.js
@@ -6,6 +6,7 @@ import "../index.css";
 function Navbar() {
     const navigate = useNavigate();
     const token = localStorage.getItem("token");
+    const role = localStorage.getItem("role");
     const handlelogout = ()=>{
         localStorage.removeItem("token");
         localStorage.removeItem("role");
@@ -17,12 +18,20 @@ function Navbar() {
             <img src={logo} alt="logo" width="100"/>    
             <div className="nav-links">
                 <Link to="/">Home</Link>
-                <Link to="/upload">Admin</Link>
+                {role==="admin" ? (
+                    <>
+                    <Link to="/upload">Admin</Link>
+                    </>
+                ):(
+                    <>
+                    </>
+                )
+                }
                 <Link to="/dashboard">Dashboard</Link>
                 {!token ? (
                     <>
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
+                    <Link className="border rounded shadow w-32 text-center" to="/login">Login</Link>
+                    <Link className=" text-center bg-purple-600 w-40 rounded hover:bg-purple-700" to="/register">Sign Up</Link>
                     </>
                 ) : (
                     <>
