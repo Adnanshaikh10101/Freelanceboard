dc7c3dd45389722b2be67a4d1ff85e6100013a5c
Adnan Shaikh <shaikhaadnan02@gmail.com>
2026-07-21 18:59:33 +0530

    Home Section Created

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,17 @@
 import "../index.css";
+import bgimg from "../assets/hero.png";
 function Home(){
     return(
-        <div className="Home">
-            
+        <div className="max-w-6xl mx-auto mt-10 border rounded shadow h-screen bg-cover p-4 flex " style={{
+            backgroundImage:`url(${bgimg})`,
+            backgroundSize:"cover",
+            backgroundPosition:"center-top" }}>
+            <div className="w-1/2 mx-1 text-fuchsia-600 border rounded shadow p-6">
+                <h1>Made Your Website</h1>
+            </div>
+             <div className="w-1/2 mx-1 text-fuchsia-600 border rounded shadow p-6">
+                <h1>Made Your Website</h1>
+            </div>   
         </div>
     );
 }
